cf2398ad73d101d5517967ecc4faa0bb4b004ad1
ryanmrichard <ryanmrichard1@gmail.com>
2018-09-05 16:12:14 -0500

    formats ERI

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,3 +1,4 @@
+#Pass to CMake with variable CMAKE_TOOLCHAIN_FILE
 set(PYTHON_EXECUTABLE /opt/rh/rh-python36/root/usr/bin/python3.6)
 set(CMAKE_CXX_COMPILER /opt/rh/devtoolset-7/root/usr/bin/g++)
 set(CMAKE_C_COMPILER /opt/rh/devtoolset-7/root/usr/bin/gcc)
