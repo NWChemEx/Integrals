fa28578e5dc1501b4121b30a03d23cfb5c40645d
Jonathan M. Waldrop <relik107@gmail.com>
2020-01-28 12:57:14 -0600

    Further update make_basis

--- integrals/nwx_libint/nwx_libint.cpp
+++ integrals/nwx_libint/nwx_libint.cpp
@@ -3,7 +3,8 @@
 namespace nwx_libint {
 
 template<typename T>
-libint2::BasisSet make_basis(const integrals::type::basis_set<T>& bs) {
+libint2::BasisSet _make_basis(const integrals::type::basis_set<T>& bs) {
+    using coord_type = std::array<double, 3>;
     using Contraction = libint2::Shell::Contraction;
     libint2::BasisSet shells;
 
@@ -11,18 +12,28 @@ libint2::BasisSet make_basis(const integrals::type::basis_set<T>& bs) {
         const auto nprims = shelli.n_unique_primitives();
         const auto first_prim = shelli.unique_primitive(0);
         const auto last_prim = shelli.unique_primitive(nprims - 1);
+        const int l = shelli.l();
+        const bool pure = shelli.pure();
 
-        coord_type<T> center = {shelli.x(), shelli.y(), shelli.z()};
+        coord_type center = {shelli.x(), shelli.y(), shelli.z()};
 
-        libint2::svector<double> alphas(&first_prim.exponent(), &last_prim.exponent());
-        libint2::svector<double> coefs(&first_prim.coefficient(), &last_prim.coefficient());
+        libint2::svector<double> alphas(&first_prim.exponent(), &last_prim.exponent() + 1);
+        libint2::svector<double> coefs(&first_prim.coefficient(), &last_prim.coefficient() + 1);
 
-        Contraction cont({shelli.l(), shelli.pure(), coefs});
+        Contraction cont({l, pure, coefs});
         libint2::svector<Contraction> conts{cont};
         shells.push_back(libint2::Shell(alphas, conts, center));
     }
 
     return shells;
+}
+
+libint2::BasisSet make_basis(const integrals::type::basis_set<double>& bs) {
+    return _make_basis(bs);
+}
+
+libint2::BasisSet make_basis(const integrals::type::basis_set<float>& bs) {
+    return _make_basis(bs);
 }
 
 } // namespace nwx_libint
--- integrals/nwx_libint/nwx_libint.hpp
+++ integrals/nwx_libint/nwx_libint.hpp
@@ -4,16 +4,13 @@
 #include <array>
 
 namespace nwx_libint {
-/** @brief Converts a libchemist::BasisSet object @p bs to a LibInt2 BasisSet
+/** @brief Converts a libchemist::AOBasisSet object @p bs to a LibInt2 BasisSet
  *         object
  *
  *  @param[in] bs The libchemist BasisSet to be converted
  *  @returns The basis set as a LibInt2 BasisSet object
  */
 
-template<typename T>
-using coord_type = std::array<T, 3>;
-
-template<typename T>
-libint2::BasisSet make_basis(const integrals::type::basis_set<T>& bs);
+libint2::BasisSet make_basis(const integrals::type::basis_set<double>& bs);
+libint2::BasisSet make_basis(const integrals::type::basis_set<float>& bs);
 } // namespace nwx_libint
