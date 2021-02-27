7aa85979b8b6784b4b2efaecd8840690768218f1
Ryan Richard <ryanmrichard1@gmail.com>
2021-02-27 14:01:18 -0600

    basic f12 integrals have been added

--- tests/integrals/f12/stg_gr.cpp
+++ tests/integrals/f12/stg_gr.cpp
@@ -12,24 +12,18 @@ TEST_CASE("STG 4 Center GR") {
     auto& world = TA::get_default_world();
     sde::ModuleManager mm;
     integrals::load_modules(mm);
-    auto mols            = testing::get_molecules();
     auto data            = testing::get_data(world);
+    auto bases           = testing::get_bases();
     const auto mol_name  = "h2";
     const auto prop_name = "STG 4C GR";
 
     SECTION(mol_name) {
-        auto mol = mols.at(mol_name);
         for(auto bs : {"sto-3g", "cc-pvdz"}) {
-            auto aos     = libchemist::apply_basis(bs, mol);
             auto tensors = data.at(mol_name).at(bs);
+            auto aos     = bases.at(mol_name).at(bs);
             auto X_corr  = tensors.at(prop_name);
             auto [X]     = mm.at(key).run_as<integral_type>(aos, aos, aos, aos);
-            // double max_diff = (X("i,j,k,l") - X_corr("i,j,k,l")).abs_max();
-            // std::cout << max_diff << std::endl;
-            // testing::print_large_deviations(X, X_corr);
-            // std::cout << std::fixed << std::setprecision(16) << X <<
-            // std::endl;
-            // REQUIRE(libchemist::ta_helpers::allclose(X, X_corr));
+            REQUIRE(libchemist::ta_helpers::allclose(X, X_corr));
         }
     }
 }
