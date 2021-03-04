b307b2db35a23ea74343b3f4ff97e0e70e0ff3f6
Jonathan M. Waldrop <relik107@gmail.com>
2021-03-03 18:02:31 -0600

    Reenable Cauchy-Schwarz screening and Screening matrix module.

--- tests/integrals/libint/test_eri3_TA.cpp
+++ tests/integrals/libint/test_eri3_TA.cpp
@@ -4,13 +4,17 @@
 
 TEST_CASE("ERI3C") {
     using integral_type = integrals::pt::eri3c<double>;
-    const auto key      = "ERI3";
+    const auto key1     = "ERI3";
+    const auto key2     = "ERI3 CS";
 
     sde::ModuleManager mm;
     integrals::load_modules(mm);
     auto [molecule, bs] = make_molecule();
-    // mm.change_input(key, "Screening Threshold", 0.000001);
-    auto [X] = mm.run_as<integral_type>(key, bs, bs, bs);
+    auto [X] = mm.run_as<integral_type>(key1, bs, bs, bs);
     TensorType X_corr(X.world(), X.trange(), corr);
     REQUIRE(libchemist::ta_helpers::allclose(X, X_corr));
+
+    mm.change_input(key2, "Screening Threshold", 0.000001);
+    auto [X2] = mm.run_as<integral_type>(key2, bs, bs, bs);
+    REQUIRE(libchemist::ta_helpers::allclose(X2, X_corr));
 }
--- tests/integrals/libint/test_yukawa4_TA.cpp
+++ tests/integrals/libint/test_yukawa4_TA.cpp
@@ -5,14 +5,18 @@
 TEST_CASE("Yukawa4C") {
     using integral_type = integrals::pt::yukawa4c<double>;
     using size_vector   = integrals::type::size_vector;
-    const auto key      = "Yukawa4";
+    const auto key1     = "Yukawa4";
+    const auto key2     = "Yukawa4 CS";
 
     sde::ModuleManager mm;
     integrals::load_modules(mm);
     auto [molecule, bs] = make_molecule();
     auto stg_exponent   = 1.0;
-    // mm.change_input(key, "Screening Threshold", 0.000001);
-    auto [X] = mm.run_as<integral_type>(key, stg_exponent, bs, bs, bs, bs);
+    auto [X] = mm.run_as<integral_type>(key1, stg_exponent, bs, bs, bs, bs);
     TensorType corr_R(X.world(), X.trange(), corr);
     REQUIRE(libchemist::ta_helpers::allclose(X, corr_R));
+
+    mm.change_input(key2, "Screening Threshold", 0.000001);
+    auto [X2] = mm.run_as<integral_type>(key2, stg_exponent, bs, bs, bs, bs);
+    REQUIRE(libchemist::ta_helpers::allclose(X2, corr_R));
 }
