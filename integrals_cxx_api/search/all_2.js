99c48c98059dd35a6f52a8c4a53422e1ae191393
ryanmrichard <ryanmrichard@users.noreply.github.com>
2026-03-31 18:51:02 +0000

    deploy: 3e3b81b3192a48166e0767e3e4c07de6edddc0c1

--- integrals_cxx_api/search/all_2.js
+++ integrals_cxx_api/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['get_5fbasis_5fsets_0',['get_basis_sets',['../namespaceintegrals_1_1libint_1_1detail__.html#a0c8c5ec1e270829eedc794bfad842c72',1,'integrals::libint::detail_']]],
+  ['get_5fbasis_5fsets_0',['get_basis_sets',['../namespaceintegrals_1_1libint_1_1detail__.html#a20dffdd5b3585afb0a639087d7b3ced7',1,'integrals::libint::detail_']]],
   ['get_5fn_1',['get_n',['../namespaceintegrals_1_1libint_1_1detail__.html#aa1c8a4a76d2c804af482511f9730cabf',1,'integrals::libint::detail_']]],
   ['get_5fpermutations_2',['get_permutations',['../namespaceintegrals_1_1utils.html#a8ad83297f2988dcdbf87468ac46dc427',1,'integrals::utils']]],
   ['get_5fpermutations_5fwith_5fsigma_3',['get_permutations_with_sigma',['../namespaceintegrals_1_1utils.html#af5e0d84d24afbb5d15aaac1240601123',1,'integrals::utils']]]
